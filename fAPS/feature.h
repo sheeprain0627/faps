ff0b13b20a8d3b8e3e88e40c9322d6ef1d62645c
kkasunperera <kkasunperera@gmail.com>
2011-06-23 11:07:07 +0000

    interpolation intregrated V1

---
+++ fAPS/feature.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "Tdsloader.h"
+class feature
+{
+public:
+	feature(void);
+	~feature(void);
+	void set3Dobject(obj_type_ptr pObject);
+
+ void changeNose(obj_type_ptr pObject, float aggesmntVa);
+
+ void changeMouth(obj_type_ptr pObject);
+
+ void changeLeftEye(obj_type_ptr pObject);
+void changeRightEye(obj_type_ptr pObject);
+
+
+float calculateGradiant(float y, float x, float change);
+};
+
+//#include "main.h"
+//#include "3ds.h"
+
+
+//t3DObject threeDobject;	
+
+/*
+class CChangefeature
+{
+public:
+void set3Dobject(obj_type_ptr pObject);
+
+ void changeNose(obj_type_ptr pObject);
+
+ void changeMouth(obj_type_ptr pObject);
+
+ void changeLeftEye(obj_type_ptr pObject);
+void changeRightEye(obj_type_ptr pObject);
+
+
+float calculateGradiant(float y, float x, float change);
+};
+*/
