e40a7c3a14cc16c6275628b2b4090a8e763ac94e
ajithsri <ajithsri@596e6c6b-c237-7fa6-b32d-8c6590fac6d7>
2011-07-08 06:09:10 +0000

    resolving conflicts

--- trunk/fAPS/MyTabThree.cpp
+++ trunk/fAPS/MyTabThree.cpp
@@ -4,6 +4,7 @@
 #include "stdafx.h"
 #include "MyTabThree.h"
 #include "afxdialogex.h"
+#include "Tdsloader.h"
 
 
 // MyTabThree dialog
@@ -27,7 +28,22 @@ void MyTabThree::DoDataExchange(CDataExchange* pDX)
 
 
 BEGIN_MESSAGE_MAP(MyTabThree, CDialog)
+	ON_BN_CLICKED(IDC_BUTTON1, &MyTabThree::OnBnClickedButton1)
+	ON_BN_CLICKED(IDC_Pick, &MyTabThree::OnBnClickedPick)
 END_MESSAGE_MAP()
 
 
 // MyTabThree message handlers
+
+
+void MyTabThree::OnBnClickedButton1()
+{
+	// TODO: Add your control notification handler code here
+}
+
+
+void MyTabThree::OnBnClickedPick()
+{
+	drawBox();
+	// TODO: Add your control notification handler code here
+}
