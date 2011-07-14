036b497ec69b6c2d707ef652306406c4ba50f11f
ajanfas <ajanfas@596e6c6b-c237-7fa6-b32d-8c6590fac6d7>
2011-07-14 07:20:10 +0000

    add Class Age progression texureEnhancePrototype(float a) create enhanced texure.

---
+++ trunk/fAPS/AgeProgression.cpp
@@ -0,0 +1,143 @@
+#include "stdafx.h"
+#include <cv.h>
+#include <cv.h>
+#include <highgui.h>
+#include "Ageprogression.h"
+
+
+Ageprogression::Ageprogression(void)
+{
+}
+
+
+Ageprogression::~Ageprogression(void)
+{
+}
+
+void Ageprogression ::texureEnhancePrototype(float p){
+	
+	IplImage* imgRed[3];
+    IplImage* imgGreen[3];
+    IplImage* imgBlue[3];
+    float q=p;
+	for(int i=0;i<3;i++)
+    {
+        IplImage* img;
+        char filename[30];
+        sprintf(filename, "Ageprogression\\%d.bmp", (i+1));
+        img = cvLoadImage(filename);
+        imgRed[i] = cvCreateImage(cvGetSize(img), 8, 1);
+        imgGreen[i] = cvCreateImage(cvGetSize(img), 8, 1);
+        imgBlue[i] = cvCreateImage(cvGetSize(img), 8, 1);
+        cvSplit(img, imgRed[i], imgGreen[i], imgBlue[i], NULL);
+        cvReleaseImage(&img);
+    }
+	
+	CvSize imgSize = cvGetSize(imgRed[0]);
+	IplImage* imgResultRed = cvCreateImage(imgSize, 8, 1);
+    IplImage* imgResultGreen = cvCreateImage(imgSize, 8, 1);
+    IplImage* imgResultBlue = cvCreateImage(imgSize, 8, 1);
+    IplImage* imgResult = cvCreateImage(imgSize, 8, 3);
+
+	for(int y=0;y<imgSize.height;y++)
+    {
+        for(int x=0;x<imgSize.width;x++)
+        {
+            int theSumRed=0;
+            int theSumGreen=0;
+            int theSumBlue=0;
+            for(int i=0;i<3;i++)
+            {
+                theSumRed+=cvGetReal2D(imgRed[i], y, x);
+                theSumGreen+=cvGetReal2D(imgGreen[i], y, x);
+                theSumBlue+=cvGetReal2D(imgBlue[i], y, x);
+            }
+            theSumRed = (float)theSumRed/3.0f;
+            theSumGreen = (float)theSumGreen/3.0f;
+            theSumBlue = (float)theSumBlue/3.0f;
+            cvSetReal2D(imgResultRed, y, x, theSumRed);
+            cvSetReal2D(imgResultGreen, y, x, theSumGreen);
+            cvSetReal2D(imgResultBlue, y, x, theSumBlue);
+        }
+    }
+	cvMerge(imgResultRed, imgResultGreen, imgResultBlue, NULL, imgResult);
+    cvSaveImage("Ageprogression\\result.bmp",imgResult );
+
+	for(int i=0;i<3;i++)
+	{ 
+	    char infilename[30];
+	    char outfilename[30];
+        sprintf(infilename, "Ageprogression\\%d.bmp", (i+1));   
+	    IplImage* img1 = cvLoadImage(infilename);
+	    IplImage* out = cvCreateImage( cvGetSize(img1), IPL_DEPTH_8U, 3 ); 
+        cvSmooth(img1, out, CV_GAUSSIAN, 7, 7,q, 0);
+	    sprintf(outfilename, "Ageprogression\\%d_smooth.bmp", (i+1));
+		cvSaveImage(outfilename, out);
+	}
+
+	/* apply Image based surface detils transfer */
+	IplImage* imgRedSmooth[3];
+    IplImage* imgGreenSmooth[3];
+    IplImage* imgBlueSmooth[3];
+	
+	for(int i=0;i<3;i++)
+	{
+	    char otfilename[30];
+        sprintf(otfilename, "Ageprogression\\%d_smooth.bmp", (i+1));
+	    IplImage* imgSmooth = cvLoadImage(otfilename);
+        imgRedSmooth[i] = cvCreateImage(cvGetSize(imgSmooth), 8, 1);
+        imgGreenSmooth[i] = cvCreateImage(cvGetSize(imgSmooth), 8, 1);
+        imgBlueSmooth[i] = cvCreateImage(cvGetSize(imgSmooth), 8, 1);
+        cvSplit(imgSmooth, imgRedSmooth[i], imgGreenSmooth[i], imgBlueSmooth[i], NULL);
+        cvReleaseImage(&imgSmooth);
+	}
+
+	IplImage* imgResultRedTem= cvCreateImage(imgSize, 8, 1);
+    IplImage* imgResultGreenTem = cvCreateImage(imgSize, 8, 1);
+    IplImage* imgResultBlueTem = cvCreateImage(imgSize, 8, 1);
+    IplImage* imgResultD = cvCreateImage(imgSize, 8, 3);
+	for(int y=0;y<imgSize.height;y++)
+    {
+        for(int x=0;x<imgSize.width;x++)
+        {
+                double theFinalRed=cvGetReal2D(imgResultRed, y, x);
+                double theFinalGreen=cvGetReal2D(imgResultGreen, y, x);
+                double theFinalBlue=cvGetReal2D(imgResultBlue, y, x);
+
+            for(int i=0;i<3;i++)
+            {
+                double  theRealRed=cvGetReal2D(imgRed[i], y, x);
+                double  theRealGreen=cvGetReal2D(imgGreen[i], y, x);
+                double theRealBlue=cvGetReal2D(imgBlue[i], y, x);
+
+			    double	theSmoothRed=cvGetReal2D(imgRedSmooth[i], y, x);
+			    double	theSmoothGreen=cvGetReal2D(imgGreenSmooth[i], y, x);
+			    double	theSmoothBlue=cvGetReal2D(imgBlueSmooth[i], y, x);
+
+			    double	theTemFinalRed=theRealRed/theSmoothRed;
+			    double	theTemFinalGreen=theRealGreen/theSmoothGreen;
+			    double	theTemFinalBlue=theRealBlue/theSmoothBlue;
+
+				theFinalRed *=theTemFinalRed;
+				theFinalGreen *=theTemFinalGreen;
+				theFinalBlue *=theTemFinalBlue;
+            }
+		  cvSetReal2D(imgResultRedTem, y, x, theFinalRed);
+          cvSetReal2D(imgResultGreenTem, y, x, theFinalGreen);
+          cvSetReal2D(imgResultBlueTem, y, x, theFinalBlue);
+        }
+    }
+	cvMerge(imgResultRedTem, imgResultGreenTem, imgResultBlueTem, NULL, imgResultD);
+    cvSaveImage("Ageprogression\\Texure-Enhanced-prototype.bmp",imgResultD);
+	cvShowImage("Texure-Enhanced-prototype", imgResultD);
+	cvWaitKey(0);
+
+}
+float Ageprogression ::getSigma()const{
+	return sigma;
+}
+void Ageprogression ::setSigma(float q){
+	sigma=q;
+}
+
+
