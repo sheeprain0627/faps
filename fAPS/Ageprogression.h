fd8f1c30afc19a8a53f6b2f608aa6705d41636d7
ajanfas <ajanfas@596e6c6b-c237-7fa6-b32d-8c6590fac6d7>
2011-07-14 07:27:01 +0000

    Agepogression headerfile added

---
+++ fAPS/Ageprogression.h
@@ -0,0 +1,14 @@
+#pragma once
+
+
+class Ageprogression
+{
+public:
+	Ageprogression(void);
+	~Ageprogression(void);
+void texureEnhancePrototype(float p);
+float getSigma()const;
+void setSigma(float q);
+private:
+	float sigma;
+};
