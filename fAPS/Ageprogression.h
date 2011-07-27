c7ca925449e95602f21588c318f35f56b89368b7
ajanfas <ajanfas@596e6c6b-c237-7fa6-b32d-8c6590fac6d7>
2011-07-27 08:16:53 +0000

    add function declaration for ibsdt (void Ageprogression ::applyIbsdt(float q))

--- fAPS/Ageprogression.h
+++ fAPS/Ageprogression.h
@@ -7,8 +7,13 @@ public:
 	Ageprogression(void);
 	~Ageprogression(void);
 void texureEnhancePrototype(float p);
+void applyIbsdt(float q);
 float getSigma()const;
 void setSigma(float q);
+//float getIbsdt()const;
+//void setIbsdt(float r);
 private:
 	float sigma;
+	//float ibsdt;
+
 };
