d014307ced3d743de5ac68df700a55169fbd3ffc
ajithsri <ajithsri@596e6c6b-c237-7fa6-b32d-8c6590fac6d7>
2011-07-28 10:02:39 +0000

    default image with ordered click point

--- fAPS/Tdsloader.h
+++ fAPS/Tdsloader.h
@@ -90,7 +90,6 @@ void makeLine();
 void changeVU(int x[],int y[],int cpoints[]);	// change the uv values from critical points selction
 void changeXYZ(int x[],int y[],int cpoints[]);
 void changeVU1(float u1,float  v1);	//change uv by considering 1st point movement
-void changeVU2(float u1,float  v1);	//change uv by a linear equation
 void drawBox();
 
 void test();
